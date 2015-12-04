433c2498768f5e7bf515be09443ab09223786e7e
David Fialho <fialho.david@gmail.com>
2015-12-04 17:19:39 +0000

    implemented server blocking communication with condition objects

--- Seleque/chat_server.py
+++ Seleque/chat_server.py
@@ -1,28 +1,44 @@
 from collections import deque
 import uuid
 import Pyro4
+from threading import Condition
 
 
 class ChatServer:
 
     def __init__(self):
+        # each client is associated to the last message he read
         self.clients = {}
+        # buffer with all the messages
+        self.messages = []
+        # condition to indicate that there is new messages
+        self.message_available = Condition
 
     def register(self):
+        # generate unique id for the new user
         client_id = uuid.uuid4()
-        self.clients[client_id] = deque()
+
+        # a new user only receives the new messages sent after registering
+        # assign to the user the index of the last message
+        self.clients[client_id] = len(self.messages)
+
         return client_id
 
-    def send_message(self, client_id, message):
-        self.clients[client_id].append(message)
+    def send_message(self, message):
+        with self.message_available:
+            self.messages.append(message)
+            self.message_available.notifyAll()
 
     def receive_message(self, client_id):
-        message = None
-        while not message:
-            try:
-                return self.clients[client_id].popleft()
-            except KeyError:
-                pass
+
+        with self.message_available:
+            client_last_index = self.clients[client_id]
+
+            while client_last_index < len(self.messages):
+                # there is a message available for the client
+                message = self.messages[self.clients[client_id]]
+                self.message_available.wait()
+
 
 Pyro4.config.SERIALIZERS_ACCEPTED = ['pickle']
 Pyro4.config.SERIALIZER = 'pickle'
