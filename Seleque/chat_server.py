5b0dca2cfac5dc96ea20b7409f5aee7efbf1cf6f
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2015-12-29 00:24:17 +0000

    Integrated the circular buffer into the server and made a simplification to the buffer.

--- Seleque/chat_server.py
+++ Seleque/chat_server.py
@@ -1,7 +1,7 @@
-from collections import deque
 import uuid
 import Pyro4
 from threading import Condition
+from circular_list import CircularList
 
 
 class ChatServer:
@@ -10,7 +10,7 @@ class ChatServer:
         # each client is associated to the last message he read
         self.clients = {}
         # buffer with all the messages
-        self.messages = []
+        self.messages_buffer = CircularList(4)
         # condition to indicate that there is new messages
         self.message_available = Condition()
 
@@ -18,30 +18,38 @@ class ChatServer:
         # generate unique id for the new user
         client_id = uuid.uuid4()
 
-        # a new user only receives the new messages sent after registering
+        # a new user only receives messages that are sent after he registers
         # assign to the user the index of the last message
-        self.clients[client_id] = len(self.messages)
+
+        self.clients[client_id] = self.messages_buffer.get_newest()[0]
 
         return client_id
 
     def send_message(self, message):
         with self.message_available:
-            self.messages.append(message)
+            self.messages_buffer.append(message)
             self.message_available.notify_all()
 
     def receive_message(self, client_id):
 
         with self.message_available:
 
+            current_index = self.clients[client_id]
+
             message = None
             while not message:
-                if self.clients[client_id] < len(self.messages):
-                    # there is a message available for the client
-                    message = self.messages[self.clients[client_id]]
-                    self.clients[client_id] += 1
-                else:
+                try:
+                    current_index, message = self.messages_buffer.get_next(current_index)
+
+                except EOFError:
                     self.message_available.wait()
 
+                except LookupError:
+                    # Data was lost
+                    print('WARNING: TEMPORARY SOLUTION. This should fetch every message '
+                          'because data was overwritten (receive_message)')
+
+        self.clients[client_id] = current_index
         return message
 
 Pyro4.config.SERIALIZERS_ACCEPTED = ['pickle']
@@ -51,7 +59,3 @@ daemon = Pyro4.Daemon()
 uri = daemon.register(ChatServer(), 'server')
 print("Ready. Object uri =", uri)
 daemon.requestLoop()
-
-
-
-
--- Seleque/circular_list.py
+++ Seleque/circular_list.py
@@ -11,6 +11,8 @@ class CircularList:
         self._data_packet = namedtuple('data_packet', 'id contents')
         self._id_packet = namedtuple('id_packet', 'index id')
 
+        self.append('Buffer created')  # Trust me, this simplifies lots of stuff :)
+
     @staticmethod
     def data_id():
         return datetime.now(timezone.utc)
@@ -38,14 +40,12 @@ class CircularList:
         :return id_packet : (namedtuple->index, id)
         :return contents : (?)
         """
-        if self._newest != -1:
-            index = self._newest
-            data_id = self._list[index].id
 
-            id_packet = self._id_packet(index=index, id=data_id)
-            return id_packet, self._list[index].contents
-        else:
-            raise LookupError('Message list is still empty')
+        index = self._newest
+        data_id = self._list[index].id
+
+        id_packet = self._id_packet(index=index, id=data_id)
+        return id_packet, self._list[index].contents
 
     def get_oldest(self):
         """
@@ -80,9 +80,10 @@ class CircularList:
         if indexed.id == id_packet.id:
             # all good, still have the referenced data, get the next message
             next_index = (id_packet.index + 1) % self._size
-            next_id = self._list[next_index].id
 
             try:
+                next_id = self._list[next_index].id
+
                 if id_packet.id < next_id:  # the next message is indeed more recent
                     id_packet = self._id_packet(index=next_index, id=next_id)
                     return id_packet, self._list[next_index].contents
