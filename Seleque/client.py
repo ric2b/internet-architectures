ac3bb241cf24ac70d5bd2f678cea3aba91d1aec9
David Fialho <fialho.david@gmail.com>
2015-12-30 19:59:13 +0000

    fixed error where the client id was assigned to the id() function this was causing the tests to raise a KeyError exception with the message <Built-in function id>

--- Seleque/client.py
+++ Seleque/client.py
@@ -1,8 +1,6 @@
 import socket
 import Pyro4
 
-from chat_server import Address
-
 Pyro4.config.SERIALIZERS_ACCEPTED = 'pickle'
 Pyro4.config.SERIALIZER = 'pickle'
 
@@ -35,7 +33,7 @@ class Client:
 
         # call the register method of the server to obtain an id and the server's address
         client_id, server_address = server.request_id()
-        
+
         # establish a TCP connection with the server
         connection = socket.socket()
         connection.connect((server_address.ip_address, server_address.port))
@@ -45,9 +43,9 @@ class Client:
 
         # wait for the server acknowledge
         ack = connection.recv(32).decode()
-        
+
         if ack == "OK":
-            self.id = id
+            self.id = client_id
             self.connection = connection
             self.server = server
         else:
@@ -62,17 +60,25 @@ class Client:
 
         self.server.send_message(message)
 
-    def recv_message(self):
+    def receive_message(self):
         """
         Receives a message from the chat server. If there is no message available, it
         blocks until a new message is available.
 
         :return: the received message.
         """
-        message = self.connection.recv(1024).decode()
 
+        self._wait_message()
         # read the message
-        return self.server.recv_message()
+        return self.server.receive_pending(self.id)
+
+    def _wait_message(self):
+        """
+        Blocks until a new message is ready to be received.
+        """
+
+        # wait for a signal from the server to unblock
+        self.connection.recv(32)
 
     def __del__(self):
         if self.connection:
