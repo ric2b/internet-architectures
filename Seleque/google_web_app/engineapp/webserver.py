436fe5085a07a6e6fece4933299751cb12b83907
David Fialho <fialho.david@gmail.com>
2016-01-05 18:10:45 +0000

    added a form to choose the date limits for the messages

--- Seleque/google_web_app/engineapp/webserver.py
+++ Seleque/google_web_app/engineapp/webserver.py
@@ -1,20 +1,32 @@
+from datetime import datetime
+
 import webapp2
 from google.appengine.ext import db
+from google.appengine.ext.webapp import template
 
 
-def get_room_messages(room_id):
+def get_room_messages(room_id, start_date=None, end_date=None):
     """
     Retrieves the all the messages in a room.
 
     :param room_id: id of the room to get the messages from.
+    :param start_date:
+    :param end_date:
     :return: list with the messages.
     :raises AttributeError: if given room id does not exist.
     """
-    room_key = db.Key.from_path('Room', room_id)
-    messages = db.GqlQuery("SELECT * FROM Message WHERE ANCESTOR is :1 ORDER BY date_time DESC", room_key)
-
-    if messages.count(limit=1) == 0:
+    room = Room.get_by_key_name([room_id])[0]
+    if room is None:
         raise AttributeError("room '%s' does not exist", (room_id,))
+
+    messages = db.GqlQuery("SELECT * FROM Message "
+                           "WHERE ANCESTOR is :room "
+                           "AND date_time >= DATETIME(:start_year, :start_month, :start_day, 0, 0, 0)"
+                           "AND date_time <= DATETIME(:end_year, :end_month, :end_day, 23, 59, 59)"
+                           "ORDER BY date_time DESC",
+                           room=room.key(),
+                           start_year=start_date.year, start_month=start_date.month, start_day=start_date.day,
+                           end_year=end_date.year, end_month=end_date.month, end_day=end_date.day)
 
     return messages
 
@@ -59,24 +71,26 @@ class CountHandler(webapp2.RequestHandler):
 
 class MessagesHandler(webapp2.RequestHandler):
     def get(self, room_id):
+        self.response.write(template.render('messages.html', {}))
+
+    def post(self, room_id):
+        start_date = str(self.request.get('start_date'))
+        end_date = str(self.request.get('end_date'))
+
+        if not start_date:
+            start_date = datetime(1, 1, 1, 0, 0, 0)
+        else:
+            start_date = datetime.strptime(start_date, '%Y-%m-%d')
+
+        if not end_date:
+            end_date = datetime.now()
+        else:
+            end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
         try:
-            messages = get_room_messages(room_id)
+            messages = get_room_messages(room_id, start_date, end_date)
+            self.response.write("<h1>Messages from %s to %s:</h1>" % (start_date.date(), end_date.date()))
             for message in messages:
-                self.response.out.write("%s : %s<br/>" % (message.text, message.date_time))
-        except AttributeError:
-            not_found_room(self.response, room_id)
-
-
-class MessagesBlockHandler(webapp2.RequestHandler):
-    def get(self, room_id, start_index, end_index):
-        start_index = int(start_index)
-        end_index = int(end_index)
-
-        try:
-            messages = get_room_messages(room_id)
-
-            self.response.write("<h1>Messages from %d to %d:</h1>" % (start_index, end_index))
-            for message in messages[start_index - 1:end_index]:
                 self.response.write("%s : %s<br/>" % (message.text, message.date_time))
                 # self.response.write("sender %s: %s<br/>" % (message.sender_id, message.text))
         except AttributeError:
@@ -91,8 +105,6 @@ class MainHandler(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     webapp2.Route('/<room_id>/count', handler=CountHandler, name='room_id'),
     webapp2.Route('/<room_id>/messages', handler=MessagesHandler, name='room_id'),
-    webapp2.Route('/<room_id>/messages/<:\d+>/<:\d+>', handler=MessagesBlockHandler),
     ('/', MainHandler),
     webapp2.Route('/<room_id>/addmessage', handler=AddMessage, name='room_id')
 ], debug=True)
-
