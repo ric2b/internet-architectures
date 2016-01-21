f91965f55bbe76e808e9e37fe08df24e2c1b795d
David Fialho <fialho.david@gmail.com>
2016-01-21 19:05:41 +0000

    ficheiro do lookup server reformatado

--- Seleque/google_web_app/lookupserver/main.py
+++ Seleque/google_web_app/lookupserver/main.py
@@ -2,15 +2,17 @@ import webapp2
 from google.appengine.ext import db
 import logging
 
+
 class RegisterServer(db.Model):
-    #room = db.StringProperty(required=True)
+    # room = db.StringProperty(required=True)
     uri = db.StringProperty(required=True)
+
 
 class AddRegisterServer(webapp2.RequestHandler):
     def get(self, room_id, uri):
         if not RegisterServer.get_by_key_name(room_id):
-        # store the new uri
-            new_rs = RegisterServer(key_name = room_id, uri=uri)
+            # store the new uri
+            new_rs = RegisterServer(key_name=room_id, uri=uri)
             new_rs.put()
 
             self.response.out.write('rs added')
@@ -26,20 +28,21 @@ class SafeAddRegisterServer(webapp2.RequestHandler):
                 result = self.add_register_server(room_id, uri)
                 commited = True
             except Exception as error:
-                logging.info(error) 
+                logging.info(error)
 
-        self.response.out.write(result)            
+        self.response.out.write(result)
 
     @db.transactional
     def add_register_server(self, room_id, uri):
         if not RegisterServer.get_by_key_name(room_id):
-        # store the new uri
-            new_rs = RegisterServer(key_name = room_id, uri=uri)
+            # store the new uri
+            new_rs = RegisterServer(key_name=room_id, uri=uri)
             new_rs.put()
 
             return 'OK'
         else:
             return 'EXISTS'
+
 
 class SeeRegisterServer(webapp2.RequestHandler):
     def get(self, room_id):
@@ -60,7 +63,7 @@ class RemRegisterServer(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
-    #webapp2.Route('/<room_id>/addrs/<uri>', handler=AddRegisterServer, name='room_id'),
+    # webapp2.Route('/<room_id>/addrs/<uri>', handler=AddRegisterServer, name='room_id'),
     webapp2.Route('/<room_id>/addrs/<uri>', handler=SafeAddRegisterServer, name='room_id'),
     webapp2.Route('/<room_id>/seers', handler=SeeRegisterServer, name='room_id'),
     webapp2.Route('/<room_id>/remrs', handler=RemRegisterServer, name='room_id')
